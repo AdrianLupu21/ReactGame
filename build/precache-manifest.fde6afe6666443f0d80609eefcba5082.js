2eeb494113a40f08164b0c8ae09670b70dc05e66
AdrianLupu21 <45321630+AdrianLupu21@users.noreply.github.com>
2019-05-07 20:56:56 +0300

    Add files via upload

---
+++ build/precache-manifest.fde6afe6666443f0d80609eefcba5082.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d288d04baa18418b01f639d38f107d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfad9e0b40f576c6a3fa",
+    "url": "/static/js/2.7d012841.chunk.js"
+  },
+  {
+    "revision": "4aea0f7ea8d7bae76c28",
+    "url": "/static/js/main.c7fec26b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
